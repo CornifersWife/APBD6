927f307a7821849c93482df1983a7537060515f2
CornifersWife <irys.latosz@gmail.com>
2024-05-06 00:01:02 +0200

    final commit

--- APBD6/APBD6/Controllers/WarehouseController.cs
+++ APBD6/APBD6/Controllers/WarehouseController.cs
@@ -1,7 +1,9 @@
+using System.Data;
 using APBD6.Models;
 using APBD6.Models.DTOs;
 using APBD6.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace APBD6.Controllers;
 
@@ -31,5 +33,36 @@ public class WarehouseController : ControllerBase {
         catch (ConflictException ex) {
             return Conflict(ex.Message);
         }
+    }
+
+    [Route("2")]
+    [HttpPost]
+    public async Task<IActionResult> AddProduct_Warehouse2(AddProductWarehouse productWarehouse) {
+        int newId;
+        await using var sqlConnection = new SqlConnection(configuration["ConnectionStrings:DefaultConnection"]);
+
+        var sqlCommand = sqlConnection.CreateCommand();
+        sqlCommand.CommandType = CommandType.StoredProcedure;
+        sqlCommand.CommandText = "AddProductToWarehouse";
+
+        sqlCommand.Parameters.AddWithValue("@IdProduct", productWarehouse.IdProduct);
+        sqlCommand.Parameters.AddWithValue("@IdWarehouse", productWarehouse.IdWarehouse);
+        sqlCommand.Parameters.AddWithValue("@Amount", productWarehouse.Amount);
+        sqlCommand.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
+
+        await sqlConnection.OpenAsync();
+
+        try {
+            newId = Convert.ToInt32(await sqlCommand.ExecuteScalarAsync());
+            return StatusCode(201, newId);
+
+        }
+        catch (Exception ex) {
+            var tmp = ex.Message;
+            if (tmp.Contains("does not exist") || tmp.Contains("no order"))
+                return NotFound(ex.Message);
+        }
+
+        return Ok();
     }
 }
